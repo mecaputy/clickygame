052021607e711b7f915f82c9a84986ed50be6b3e
mecaputy <37423088+mecaputy@users.noreply.github.com>
2018-08-14 13:51:57 -0400

    Update PictureCard.js

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -6,9 +6,9 @@ import "./PictureCard.css";
 const PictureCard = props => (
   <Col size="md-3" >
     <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
-    <div className="img-container">
+//     <div className="img-container">
       <img className="click-item" alt={props.name} src={props.image}/>
-    </div>
+//     </div>
     </div>
   </Col>
   
